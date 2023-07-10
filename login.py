57d3413827a2ec25f1325f4feb6a0e08f689b896
Lordekai <alichowdhurya@gmail.com>
2023-07-10 18:01:06 -0400

    feat: First login API
    Creating the first backend Flask API for the WayneGain application

---
+++ login.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/register', methods=['GET'])
+def register():
+    data = request.get_json()
+    # Extract user registration details from the request data
+    username = data['username']
+    password = data['password']
+    # Perform validation and store user details in the database
+
+    return jsonify({'message': 'User registered successfully'})
+
+
+@app.route('/api/login', methods=['GET'])
+def login():
+    data = request.get_json()
+    # Extract user login credentials from the request data
+    username = data['username']
+    password = data['password']
+    # Perform validation and authenticate user against stored credentials
+
+    return jsonify({'message': 'User logged in successfully'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
