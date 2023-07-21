170f1c8a42b7256a253f1b2a58778649bca98d57
Sai Kanaparthi <66964100+GHOST0000-AI@users.noreply.github.com>
2023-07-20 20:41:06 -0400

    Add files via upload
    Daily goals

---
+++ Personal Goals.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+import random
+
+app = Flask(__name__)
+
+daily_goals = [
+    "Read for 30 minutes",
+    "Drink 2 liters of water",
+    "Walk 10,000 steps",
+    "Learn something new",
+    "Get 2 workouts done in a day",
+    "Eat Healthy 6/7 days of a week",
+    "Try to read 5 pages a day",
+    "Keep my room clean!"
+
+]
+
+@app.route('/random_goal', methods=['GET'])
+def get_random_goal():
+    return jsonify({"goal": random.choice(daily_goals)}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
